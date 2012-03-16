1d1bb163e6514aa55a7ffa9d799f8e8e6b2bb267
= <yigitsadic@gmail.com>
2012-03-16 21:24:27 +0200

    bağlantılar

--- links/views.py
+++ links/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render, get_object_or_404
+
+from pythontr_org.links.models import Link
+
+
+def index(request):    
+    links = Link.objects.all()
+    
+    return render(request, 'links/index.html', locals())
+
+
+def show(request, id):
+    link = get_object_or_404(Link, id=id)
+    
+    return render(request, 'links/show.html', locals())
