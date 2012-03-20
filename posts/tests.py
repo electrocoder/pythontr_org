6bc3e22938c03bfcbdfafea6c0eb11ea518f964d
= <yigitsadic@gmail.com>
2012-03-21 01:18:03 +0200

    testleri düzenliyorum

--- posts/tests.py
+++ posts/tests.py
@@ -13,109 +13,116 @@ from pythontr_org.posts.models import Post, Category
 
 class PostFunctionals(TestCase):
     """
-        'Post' modeli ile ilgili olan fonksiyonları test
-        eder.
-        'index', 'show', 'search'
+    
+        'Post' modeli ile ilgili functional test.    
+        
     """
     
     fixtures = ['posts.json', 'auth.json']        
     
     def test_get_index(self):
         """
-        Index sayfasına istek gönderir.
-        200 durum kodu döndürmelidir.
+        
+            Index sayfasına istek gönderir.
+            200 durum kodu döndürmelidir.
+            
         """
         
         response = self.client.get(reverse('posts:index'))
-        self.assertEqual(response.status_code, 200, 'Gönderiler: posts:index sayfasının testinde durum kodu uyuşmuyor!')
         
-        self.assertIsNotNone(response.context['posts'], 'Gönderiler: posts:index sayfasında gönderi yok!')
+        self.assertEqual(response.status_code, 200)        
+        self.assertIsNotNone(response.context['object_list']) # generic view
+        
         
     def test_get_show(self):
         """
-        Show sayfasına istek gönderir.
-        200 durum kodu döndürmelidir.
+        
+            Show sayfasına istek gönderir.
+            200 durum kodu döndürmelidir.
+            
         """
         
         posts = Post.objects.filter(published = True)
         
         for post in posts:
-            response = self.client.get(reverse('posts:show', args = [post.id, post.slug]))
+            response = self.client.get(post.get_absolute_url())
             
-            self.assertEqual(response.status_code, 200, 'Gönderiler: Gönderilerden biri gösterilirken hata oluştu.')
-            self.assertEqual(response.context['post'].title, post.title, 'Gönderiler: Gönderilerden biri yok!')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.context['post'].title, post.title)
             
-            self.assertEqual(post.read_count + 1, response.context['post'].read_count, 'Gönderiler: Gönderinin okunma sayısı artmadı.')
+            self.assertEqual(post.read_count + 1, response.context['post'].read_count)
             
             
     def test_get_show_should_return_404(self):
         """
-        Show sayfasına istek gönderir.
-        Yanlış bir id ile istek yapılır.
-        404 hata kodu döndürmelidir.
+        
+            Show sayfasına istek gönderir.
+            Yanlış bir id ile istek yapılır.
+            404 hata kodu döndürmelidir.
+            
         """
         
-        BAD_IDS = [i for i in range(10, 11)]
+        BAD_IDS = ['wrong-slug-one', 'wrong-and-bad-slug']
         
         for bad in BAD_IDS:
-            response = self.client.get(reverse('posts:show', args=[bad, 'wrong!']))
+            response = self.client.get(reverse('posts:show', args=['django', bad]))
             
-            self.assertEqual(response.status_code, 404, 'Gönderiler: 404 Hata kodu döndürmedi!')
+            self.assertEqual(response.status_code, 404)
             
             
     def test_should_search(self):
         """
-        Search sayfasına istek gönderir.
-        django ve flask için arama yapar.
+        
+            Search sayfasına istek gönderir.
+            django ve tkinter için arama yapar.
+            
         """
+                
+        SEARCH_LIST = ('django', 'tkinter')
         
-        SEARCH_DICT = {
-                       'django': [Post.objects.get(pk = 1), Post.objects.get(pk=2)],
-                       'flask': [Post.objects.get(pk=4)],
-        }
+        for q in SEARCH_LIST:
+            response = self.client.get(reverse('posts:search'), {'q': q})
+            
+            self.assertIsNotNone(response.context['posts'])
         
-        # django için ara
-        
-        response = self.client.get(reverse('posts:search'), {'q': 'django'})
-        for p in response.context['posts']:
-            self.assertTrue(p in SEARCH_DICT['django'], 'Gönderiler: Django için arama sonuçlarında hata var!')
-        
-        # flask için ara
-        
-        response = self.client.get(reverse('posts:search'), {'q': 'flask'})
-        self.assertIsNotNone(response.context['posts'], 'Gönderiler: Arama sonucu flask için boş döndü!')
-        
+
         
 class CategoryFunctionals(TestCase):
     """
+    
         'Category' modeli ile ilgili functional test.
+        
     """
     
     fixtures = ['posts.json']        
     
     def test_should_get_show_category(self):
         """
+        
             Her kategorideye bağlı gönderileri listeleme
             testi.
+            
         """
         
-        categories = Category.objects.filter(pk__in = [1, 3, 4])
+        categories = Category.objects.all()
         
         for cat in categories:
-            response = self.client.get(reverse('posts:show_category', args=[cat.id, cat.slug]))
+            response = self.client.get(cat.get_absolute_url())
             
-            self.assertEqual(response.status_code, 200, 'Kategoriler: Kategori gösterme başarı ile sonuçlanmadı.')
-            self.assertIsNotNone(response.context['posts'], 'Kategoriler: Verilen kategoride gönderi yok!')
+            self.assertEqual(response.status_code, 200)
+            self.assertIsNotNone(response.context['posts'])
         
             self.assertEqual(len(cat.post_set.all()), len(response.context['posts']))
             
             
     def test_should_raise_404_with_bad_id(self):
         """
+        
             Yanlış id verilirse 404 döndürmelidir.
+            
         """
         
-        response = self.client.get(reverse('posts:show_category', args=[123, 'hatali']))
+        response = self.client.get(reverse('posts:show_category', args=['hatali']))
         
-        self.assertEqual(response.status_code, 404, 'Kategoriler: 404 vermesi gereken yerde bir hata var!')
+        self.assertEqual(response.status_code, 404)
          
