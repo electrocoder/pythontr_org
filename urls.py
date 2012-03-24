6c5c54cc97e28ffc48ef3bc1c7dd75ecbf854582
= <yigitsadic@gmail.com>
2012-03-24 15:18:20 +0200

    url değişiklikleri

--- urls.py
+++ urls.py
@@ -1,39 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls.defaults import patterns, include, url
-
 from django.conf import settings
+
 from django.contrib import admin
-
-from django.views.generic.simple import redirect_to
-
-from django.conf.urls.defaults import *
-
+admin.autodiscover()
 
 # STATIC ve MEDIA dosyaları için
+
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from pythontr_org.extra.feeds import RSS_URLS
 from pythontr_org.extra.sitemaps import SITEMAPS_URLS
 
-admin.autodiscover()
-
 
 urlpatterns = patterns('',
-                       url(r'^$', redirect_to, {'url': '/py/'}),
+                       url(r'^', include('pythontr_org.posts.urls', namespace = 'posts')),
+                       
                        url(r'^errors/access_denied/$', 'pythontr_org.extra.views.access_denied', name = 'access_denied'),
-                       
                        url(r'^accounts/became_an_author/$', 'pythontr_org.extra.views.became_an_author', name = 'became_an_author'),
-                       
                        url(r'^contact/$', 'pythontr_org.extra.views.contact', name = 'contact'),
                        
                        url(r'^links/', include('pythontr_org.links.urls', namespace = 'links')),                                              
-                       url(r'^py/', include('pythontr_org.posts.urls', namespace = 'posts')),    
-                       url(r'^polls/', include('pythontr_org.polls.urls', namespace = 'polls')),                       
-                       url(r'^admin/', include(admin.site.urls)),   
+                       url(r'^polls/', include('pythontr_org.polls.urls', namespace = 'polls')),
+                       
                        url(r'^accounts/', include('pythontr_org.users.urls', namespace = 'users')),
-                       url(r'^tinymce/', include('tinymce.urls')),               
+                       
+                       url(r'^tinymce/', include('tinymce.urls')),
+                       
+                       url(r'^admin/', include(admin.site.urls)),               
 )
 
 urlpatterns += SITEMAPS_URLS 
