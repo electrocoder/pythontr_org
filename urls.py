69f7c9358a335fa14d8c9a1bbc40c67ec8c02256
= <yigitsadic@gmail.com>
2012-03-24 22:30:34 +0200

    url değişiklikleri

--- urls.py
+++ urls.py
@@ -15,9 +15,7 @@ from pythontr_org.extra.feeds import RSS_URLS
 from pythontr_org.extra.sitemaps import SITEMAPS_URLS
 
 
-urlpatterns = patterns('',
-                       url(r'^', include('pythontr_org.posts.urls', namespace = 'posts')),
-                       
+urlpatterns = patterns('',                       
                        url(r'^errors/access_denied/$', 'pythontr_org.extra.views.access_denied', name = 'access_denied'),
                        url(r'^accounts/became_an_author/$', 'pythontr_org.extra.views.became_an_author', name = 'became_an_author'),
                        url(r'^contact/$', 'pythontr_org.extra.views.contact', name = 'contact'),
@@ -29,7 +27,9 @@ urlpatterns = patterns('',
                        
                        url(r'^tinymce/', include('tinymce.urls')),
                        
-                       url(r'^admin/', include(admin.site.urls)),               
+                       url(r'^admin/', include(admin.site.urls)),
+                          
+                       url(r'^', include('pythontr_org.posts.urls', namespace = 'posts')),            
 )
 
 urlpatterns += SITEMAPS_URLS 
