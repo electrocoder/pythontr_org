9b146f60c3096674d38e0527cc3bab011e6a0759
= <yigitsadic@gmail.com>
2012-04-05 23:21:28 +0300

    author protected view

--- utils.py
+++ utils.py
@@ -7,6 +7,8 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 from django.views.generic import View
+from django.core.urlresolvers import reverse
+
 
 def slugify_unicode(value):
     """
@@ -25,3 +27,13 @@ class ProtectedView(View):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(ProtectedView, self).dispatch(*args, **kwargs)
+    
+    
+class AuthorProtectedView(ProtectedView):
+    def dispatch(self, *args, **kwargs):
+        post = self.get_queryset()
+        
+        if not post.author != self.request.user:
+            return redirect('access_denied')
+        
+        return super(AuthorProtectedView, self).dispatch(*args, **kwargs)
