c67960b4cbce1f03794e2a260da68539424ea96c
= <yigitsadic@gmail.com>
2012-04-05 04:08:02 +0300

    links new -> class based generic view

--- links/urls.py
+++ links/urls.py
@@ -6,6 +6,5 @@ from pythontr_org.links.views import LinkListView, NewLinkView
 
 urlpatterns = patterns('pythontr_org.links.views',
                        url(r'^$', LinkListView.as_view(), name='index'),
-                       url(r'^new/$', 'new', name='new'),
-                       url(r'^new2/$', NewLinkView.as_view())
+                       url(r'^new/$', NewLinkView.as_view(), name='new')
 )
--- links/views.py
+++ links/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from django.core.urlresolvers import reverse
 
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 from django.views.generic import ListView
 from django.views.generic import CreateView
 
@@ -21,37 +23,24 @@ class LinkListView(ListView):
 
 
 class NewLinkView(CreateView):
-    success_url = '/'
     template_name = 'links/new.html'
     
-    def get_form(self):
-        return LinkForm(self.request.POST or None, instance = Link(confirmed=False))
+    form_class = LinkForm
+    initial = {
+               'href': 'http://'
+    }
+    
     
     def form_valid(self, form):
-        link = LinkForm(self.request.POST)
+        link = LinkForm(self.request.POST, instance=Link(confirmed=False))
         link.save()
-        
-        #LinkAddedMail(link)
-        
-        messages.success(self.request, u'Teşekkürler. Bağlantı başarı ile eklendi. Yöneticiler onayladığında listede yerini alacaktır.')
-        
-
-
-@login_required
-def new(request):
-    """
-        Yeni bağlantı eklemek için kullanılır.
-        Bağlantı kaydedilir, yöneticilere mail gönderilir.
-    """
-    
-    form = LinkForm(request.POST or None, instance = Link(confirmed = False))
-    
-    if form.is_valid():
-        link = form.save()
         
         LinkAddedMail(link)
         
-                
+        messages.success(self.request, u'Teşekkürler. Bağlantı başarı ile eklendi. Yöneticiler onayladığında listede yerini alacaktır.')
         return redirect('users:settings')
     
-    return render(request, 'links/new.html', locals())
+    
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(NewLinkView, self).dispatch(*args, **kwargs)
